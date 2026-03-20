f4d5f7f17b0c1dee6681423cbfea8b31909910d4
hsikelias <plekishsai@gmail.com>
2026-03-20 16:59:01 -0400

    Week 3 Problem 1: fuel completed

--- Week_3/ProblemSet3/fuel.py
+++ Week_3/ProblemSet3/fuel.py
@@ -12,16 +12,25 @@ def verify_input(fraction):
 
     if fraction.count("/") == 1:
         split_fraction = fraction.split('/') # [5,6]
-        x, y = split_fraction # [x = 5, y = 6]
-        int_x = int(x)
-        int_y = int(y)
+        x, y = split_fraction # [x ="5", y = "6"]
 
+        if len(split_fraction) == 2:
+            try:
+                int_x = int(x)
+                int_y = int(y)
 
-        if len(split_fraction) == 2 and x.isdigit() and y.isdigit():
-            fraction_answer(int_x, int_y)
+                if int_x < 0 or int_y < 0:
+                    main()
+                    raise ValueError
+
+                fraction_answer(int_x, int_y)
+            except ValueError:
+                print("Follow the proper format")
+                main()
         else:
-            raise ValueError
+            print("Follow the proper format")
             main()
+            raise ValueError
 
     else:
         print("Enter proper format again")
@@ -43,13 +52,17 @@ def fraction_answer(x,y):
             return "E"
         else:
             get_percentage(percent)
-
-    elif x > y:
+    
+    if y == 0:
+        print("y can't be 0")
+        main()
+        raise ZeroDivisionError
+    if x > y:
         print("X is greater than Y, X should be smaller than Y")
         main()
-    elif y == 0:
-        print("Y can't be 0, try again")
-        main()
+   #  elif y == 0:
+     #   print("Y can't be 0, try again")
+      #  main()
 
 
 def get_percentage(answer):
@@ -61,30 +74,5 @@ main()
 
 # Tests
 '''
-:) input of 3/4 yields output of 75%
-:) input of 1/3 yields output of 33%
-:) input of 2/3 yields output of 67%
-:( input of 0/100 yields output of E
-    expected: "E"
-    actual:   ""
-:( input of 1/100 yields output of E
-    expected: "E"
-    actual:   ""
-:( input of 100/100 yields output of F
-    expected: "F"
-    actual:   ""
-:( input of 99/100 yields output of F
-    expected: "F"
-    actual:   ""
-:) input of 100/0 results in reprompt
-:) input of 10/3 results in reprompt
-:( input of three/four results in reprompt
-    expected program to reject input, but it did not
-:( input of 1.5/4 results in reprompt
-    expected program to reject input, but it did not
-:( input of 3/5.5 results in reprompt
-    expected program to reject input, but it did not
-:) input of 5-10 results in reprompt
-:) input of -1/4 results in reprompt
-
+input of -1/4 results in reprompt, needs to reject
 '''
