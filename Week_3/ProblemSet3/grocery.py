e653decc0ce690aa6dffe9d94938b5a189c4cba8
hsikelias <plekishsai@gmail.com>
2026-03-24 10:27:07 -0400

    Completed Grocery Problem, 1 more to go

---
+++ Week_3/ProblemSet3/grocery.py
@@ -0,0 +1,21 @@
+item_list = []
+used = []
+
+
+def main():
+    while True:
+        try:
+            item = input().upper()
+            item_list.append(item)
+        except:
+            EOFError
+            break
+
+    for item in item_list:
+        if item not in used:
+            item_count = item_list.count(item)
+            print(f"{item_count} {item}")
+            used.append(item)
+
+
+main()
