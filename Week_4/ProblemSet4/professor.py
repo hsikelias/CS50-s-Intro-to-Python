509c4d7ab495c1c28d2aa3f4b4b1178f1194b5f4
hsikelias <plekishsai@gmail.com>
2026-04-09 22:55:59 -0400

    Daily commit

--- Week_4/ProblemSet4/professor.py
+++ Week_4/ProblemSet4/professor.py
@@ -1,46 +1,63 @@
 import random
-import sys
 
 
 def main():
-    qn_count = 10
-    attempts = 0
-    level = get_level()
+    questions = 10
+    correct = 0
+    n = get_level()
 
-    while qn_count > 10:
-        x = generate_integer(level)
-        y = generate_integer(level)
-        correct_answer = x + y
-        try:
-            user_answer = int(input(f"{x} + {y}= "))
-            if user_answer == correct_answer:
-                qn_count -= 1
-            else:
+    while questions > 0:
+        x = generate_integer(n)
+        y = generate_integer(n)
+        assert x is not None
+        assert y is not None
+        answer = x + y
+        chance = 3
+
+        while chance > 0:
+            try:
+                user_ans = int(input(f"{x} + {y} = "))
+                if user_ans == answer:
+                    correct += 1
+                    break
+                else:
+                    print("EEE")
+                    chance -= 1
+            except ValueError:
                 print("EEE")
-                attempts += 1
-        except ValueError:
-            print("EEE")
-            attempts += 1
+                chance -= 1
+
+        if chance == 0:
+            print(f"{x} + {y} = {answer}")
+        questions -= 1
+
+    print(correct)
 
 
 def get_level():
-    try:
-        n = int(input("Level: "))
-        if n == 1 or n == 2 or n == 3:
-            return n
-    except ValueError:
-        pass
+    """prompts user for level and returns it"""
+    while True:
+        try:
+            n = int(input("Level: "))
+
+            if n in [1, 2, 3]:
+                return n
+        except ValueError:
+            pass
 
 
-def generate_integer(number_count):
-    if number_count == 1:
-        return random.randint(0, 9)
-    elif number_count == 2:
-        return random.randint(10, 99)
-    elif number_count == 3:
-        return random.randint(100, 999)
-    else:
-        raise ValueError("Invalid Level entered")
+def generate_integer(level):
+    """returns a single randomly generated non-negative integer with level digits"""
+    # assume level = 3
+    if level == 1:
+        int = random.randint(0, 9)
+        return int
+    elif level == 2:
+        int = random.randint(10, 99)
+        return int
+    elif level == 3:
+        int = random.randint(100, 999)
+        return int
 
 
 if __name__ == "__main__":
